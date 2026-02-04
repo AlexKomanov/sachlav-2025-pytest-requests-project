c70acfb1a67fd2cc8e24305e93da365b0773f59c
AlexKomanov <akomanov88@gmail.com>
2026-02-04 19:00:02 +0200

    pytest example files

---
+++ test_demo_1.py
@@ -0,0 +1,15 @@
+# Just to present naming convension of test file naming and structure
+
+def test_example_1():
+    pass
+
+# incorrect named test function
+def example_test():
+    pass
+
+# incorrect named test function
+def validate_test_case():
+    pass
+
+def test_example_2():
+    pass
